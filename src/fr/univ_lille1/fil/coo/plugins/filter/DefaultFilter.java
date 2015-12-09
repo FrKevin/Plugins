3ee170f49a020c15a3a6cbc0d70d4e0cfa950271
Kevin <kevin-gamelin@hotmail.fr>
2015-12-09 11:12:19 +0100

    rm sysout

--- src/fr/univ_lille1/fil/coo/plugins/filter/DefaultFilter.java
+++ src/fr/univ_lille1/fil/coo/plugins/filter/DefaultFilter.java
@@ -18,29 +18,22 @@ public class DefaultFilter implements FilenameFilter {
 		}
 		Class<?> c = getClass(toClassName(name));
 		if(c == null ){
-			System.out.println("c == null ");
 			return false;
 		}
-		System.out.println("prout class c toString  "+ c.toString());
 		return  classInPackagePlugin(c) && classContructorsNoParameters(c) && inherFromPlugin(c);
 	}
 	
 	protected String toClassName(String name){
-		System.out.println("toClassName() 1 "+ name );
 		String classname = name.replaceFirst("\\.class$", "");
-		System.out.println("toClassName() 2 "+ classname );
 		return NAME_OF_PACKAGE + "." + classname;
 	}
 	
 	
 	protected Class<?> getClass(String className){
 		try { 
-			System.out.println("getClass() " + className);
 			Class<?> c = Class.forName(className);
-			System.out.println(c);
 			return c;
 		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
 			return null;
 		}
 	}
@@ -51,13 +44,10 @@ public class DefaultFilter implements FilenameFilter {
 		
 	
 	protected boolean classInPackagePlugin(Class<?> c){
-		System.out.println("prout classInPackagePlugin()");
-		System.out.println(" c= "+ c);
 		Package p = c.getPackage();
 		if(p == null ) {
 			return false;
 		}
-		System.out.println("prout classInPackagePlugin() 2 "+ p);
 		return c.getPackage().getName().equals(NAME_OF_PACKAGE);
 	}
 	
@@ -66,7 +56,6 @@ public class DefaultFilter implements FilenameFilter {
 	}
 	
 	protected boolean classContructorsNoParameters(Class<?> c){
-		System.out.println("prout classContructorsNoParameters()");
 		Constructor<?>[] cos = c.getConstructors();
 		for(Constructor<?> constructor : cos){
 			if (constructor.getParameterCount() == 0){
